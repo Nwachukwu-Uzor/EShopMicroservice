32dde96afdbe6bdb8307fcb7af8bfd6e9c945a7a
¨Nwachukwu-Uzor¨ <uzor1997@gmail.com>
2024-11-07 20:06:20 +0100

    add: marten transactional document database

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,3 @@
-using BuildingBlocks.CQRS;
-using CatalogAPI.Models;
-using MediatR;
-
 namespace CatalogAPI.Products.CreateProduct;
 
 public record CreateProductCommand(
@@ -13,7 +9,7 @@ public record CreateProductCommand(
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -27,7 +23,9 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
             Category = command.Category,
         };
         // save product to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
         // return result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
