321097824c4b6cdcbd2b0c6cbf6437bfe5f2e131
Nwachukwu-Uzor <uzor1997@gmail.com>
2024-11-23 20:05:48 +0100

    add: efcore with sqlite for data access in discount service

--- src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Discount.Grpc.Data
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
 {
-    public class DiscountDbContext
+    public class DiscountDbContext : DbContext
     {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+        public DiscountDbContext(DbContextOptions<DiscountDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
