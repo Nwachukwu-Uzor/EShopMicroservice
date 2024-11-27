4d4f8de5fc5784f208ab8930ae6a281b2eb9d2a1
¨Nwachukwu-Uzor¨ <uzor1997@gmail.com>
2024-11-27 15:35:42 +0100

    add: initial dependency inject across ordering services

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,12 @@
+using Ordering.API.Extensions;
+using Ordering.Application.Extensions;
+using Ordering.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddApiServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApplicationServices();
 var app = builder.Build();
 
 app.Run();
